75b2c0962e4105c000f9f52f1eaf94b1425695d4
LAPTOP-V21CB5S8\Think <wangzeka@sheridancollege.ca>
2025-10-28 15:02:28 -0400

    Made a timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro; 
+
+public class Timer : MonoBehaviour
+{
+
+    public TMP_Text timerText;
+    
+    public float timer; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+        timer = 5; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        countDown();
+        
+    }
+
+    public void countDown()
+    {
+        
+        if (timer > 0)
+        {
+
+            timer -= Time.deltaTime;
+            timerText.text = "Time: " + timer.ToString("0"); 
+
+        }
+
+
+    }
+}
