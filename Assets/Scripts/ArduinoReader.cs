430440e6f1ccd4da92440400676714066b7c55ec
jhgh <92657664+stapleshgh@users.noreply.github.com>
2025-10-28 13:55:40 -0400

    arduino interface with unity

---
+++ Assets/Scripts/ArduinoReader.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoReader : MonoBehaviour
+{
+    SerialPort serial = new SerialPort("COM11", 9600);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        serial.Open();
+        serial.ReadTimeout = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = serial.ReadLine();
+        int value = int.Parse(data);
+        Debug.Log(value);
+    }
+}
