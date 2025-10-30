98d2b7931502aa376f5909c1707350b8d0365221
TheSuperfon <astefanadon@gmail.com>
2025-10-30 14:14:55 -0400

    GameOverUpdated

--- Assets/Scripts/GameOverCondition.cs
+++ Assets/Scripts/GameOverCondition.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 public class GameOverCondition : MonoBehaviour
 {
 
-    public Counter verdictScript; 
+    public Counter verdictScript;
+    public float badVerdictMaximum = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,7 +27,7 @@ public class GameOverCondition : MonoBehaviour
         int badV = verdictScript.badVCount; 
         int goodV = verdictScript.goodVCount; 
 
-        if (badV - goodV > 5)
+        if (badV >= badVerdictMaximum)
         {
 
             SceneManager.LoadScene("GameOver");
